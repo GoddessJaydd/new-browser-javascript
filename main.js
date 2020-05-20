47687023c9b6d9f890e8e18873d7d9e551394189
Goddess Joelle <r.jonellealleyne@gmail.com>
2020-05-20 11:45:46 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// for (var i =  0; i < 12; i++) {
+//   console.log('ping', i);
+// }
+// var j = 0 
+
+// while (j < 12) {
+//   console.log(' pong', j);
+
+//   j++;
+// }
+var n = 15;
+ console.log('n is', n);
+
+ while (n < 20){
+   console.log('n is', n );
+   n++;
+ }
+
+ do{ 
+   console.log('n is', n);
+   n++;
+ }while (n < 21);
